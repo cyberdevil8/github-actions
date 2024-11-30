4748e576babcfaed3d719906bfd58b73dae90563
Amit Potdar <amitpotdar@Amits-MacBook-Air.local>
2024-11-30 19:35:41 +0530

    git actions deployment pipeline creation

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+    description = "AWS region to deploy resources"
+    type        =  string
+    default     = "us-east-1"
+}
+
+variable "lambda_function_name" {
+    description = "Name of the lambda function"
+    type        = string
+    default     = "hello-world-lambda"
+}
