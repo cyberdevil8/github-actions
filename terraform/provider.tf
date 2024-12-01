5094cc963919ee84a2538396156593a1bfddd63f
Amit Potdar <amitpotdar@Amits-MacBook-Air.local>
2024-12-01 21:40:54 +0530

    fix terraform scripts and add requirements

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,13 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~4.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0" # Correct syntax
     }
+  }
+  required_version = ">= 1.5.5"
 }
 
 provider "aws" {
-    region = var.region
+  region = var.aws_region
 }
