a56121fa646e788ec8fc4c2781277a32b289a08d
Amit Potdar <amitpotdar@Amits-MacBook-Air.local>
2024-12-02 15:02:00 +0530

    last commit22

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,3 @@
-resource "aws_s3_bucket" "lambda_bucket" {
-  bucket = var.s3_bucket_name
-  acl    = "private"
-
-  force_destroy = true
-}
-
 # Reference existing IAM role
 data "aws_iam_role" "existing_lambda_role" {
   name = "hello_world_github_actions"
@@ -14,7 +7,7 @@ data "aws_iam_role" "existing_lambda_role" {
 resource "aws_lambda_function" "hello_world" {
   function_name = "hello_world_lambda"
 
-  s3_bucket = aws_s3_bucket.lambda_bucket.bucket
+  s3_bucket = var.s3_bucket_name
   s3_key    = "lambda_function.zip"
 
   runtime = "python3.9"
